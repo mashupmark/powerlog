7600440f90dc1753c75a2bfe69079b8060d4d262
Mashupmark <mashupmark@kabelbw.de>
2026-03-25 15:39:57 +0100

    feat: add offline capabilities

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,36 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
+  modules: ["@vite-pwa/nuxt"],
   runtimeConfig: {
     couchDbURL: "",
     couchDbUser: "",
     couchDbPassword: "",
+  },
+  vite: {
+    optimizeDeps: {
+      include: ["pouchdb"],
+    },
+  },
+
+  pwa: {
+    manifest: {
+      name: "Powerlog",
+      short_name: "Powerlog",
+      description: "Easily track your working hours",
+    },
+    registerType: "autoUpdate",
+    workbox: {
+      navigateFallback: "/",
+      globPatterns: ["**/*.{js,css,html,json,svg,webp}"],
+      globIgnores: ["/db/**"],
+    },
+  },
+
+  nitro: {
+    prerender: {
+      // It's neccessary to prerender the index route so it can be cached for offline access
+      routes: ["/"],
+    },
   },
 });
