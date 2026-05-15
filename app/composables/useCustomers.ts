2e8f5bfd1d30faf52d41afe39e5b6402b4b16a22
Mashupmark <mashupmark@kabelbw.de>
2026-05-15 16:23:37 +0200

    fix: make sure always all customers / projects are querried

--- app/composables/useCustomers.ts
+++ app/composables/useCustomers.ts
@@ -9,6 +9,7 @@ export const useCustomersQuery = () => {
       const customers: PouchDB.Find.FindResponse<Pick<Log, "customerName">> = await $db.find({
         fields: ["customerName"],
         selector: { customerName: { $exists: true } },
+        limit: Infinity,
       });
       return Array.from(new Set(customers.docs.map(({ customerName }) => customerName!)));
     },
