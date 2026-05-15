9f433b8ca4031688b0859da76e037c6ad847310a
Mashupmark <mashupmark@kabelbw.de>
2026-05-15 14:17:56 +0200

    feat: allow logs to be started & stopped from recent projects

---
+++ app/composables/useCurrentLog.ts
@@ -0,0 +1,57 @@
+import { DateTime } from "luxon";
+
+export type CurrentLog = { startedAt: string; customerName?: string; projectName?: string };
+export const useCurrentLog = () => {
+  const localStorageKey = "currentLog";
+  const currentLog = ref<CurrentLog | undefined>();
+
+  onBeforeMount(() => {
+    const log = localStorage.getItem(localStorageKey);
+    if (log == null) {
+      currentLog.value = undefined;
+      return;
+    }
+
+    let parsedLog: unknown;
+    try {
+      parsedLog = JSON.parse(log);
+    } catch {
+      currentLog.value = undefined;
+      localStorage.removeItem(localStorageKey);
+    }
+
+    if (
+      !!parsedLog &&
+      typeof parsedLog === "object" &&
+      "startedAt" in parsedLog &&
+      typeof parsedLog.startedAt === "string" &&
+      DateTime.fromISO(parsedLog.startedAt).isValid
+    ) {
+      const validatedLog: CurrentLog = { startedAt: parsedLog.startedAt };
+      if (
+        "customerName" in parsedLog &&
+        typeof parsedLog.customerName === "string" &&
+        parsedLog.customerName.length > 0
+      ) {
+        validatedLog.customerName = parsedLog.customerName;
+      }
+
+      if ("projectName" in parsedLog && typeof parsedLog.projectName === "string" && parsedLog.projectName.length > 0) {
+        validatedLog.projectName = parsedLog.projectName;
+      }
+
+      currentLog.value = validatedLog;
+    }
+  });
+
+  watch(
+    currentLog,
+    (log) => {
+      if (log == undefined) localStorage.removeItem(localStorageKey);
+      else localStorage.setItem(localStorageKey, JSON.stringify(log));
+    },
+    { deep: true },
+  );
+
+  return currentLog;
+};
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,39 @@
 <script setup lang="ts">
-const { data: recentProjects } = useRecentProjectsQuery();
+import { iconMediaPause, iconMediaPlay, iconMediaStop } from "@sit-onyx/icons";
 
 const recentProjectsId = useId();
+const { $db } = useNuxtApp();
+
+const currentLog = useCurrentLog();
+const { data: recentProjects } = useRecentProjectsQuery();
+
+const isCurrentLog = (recentProject: { customerName?: string; projectName?: string }) => {
+  return (
+    currentLog.value?.customerName === recentProject.customerName &&
+    currentLog.value?.projectName === recentProject.projectName
+  );
+};
+
+const startLogging = (initalOptions?: { customerName?: string; projectName?: string }) => {
+  currentLog.value = {
+    startedAt: new Date().toISOString(),
+    customerName: initalOptions?.customerName,
+    projectName: initalOptions?.projectName,
+  };
+};
+
+const stopLogging = async () => {
+  if (currentLog.value === undefined) return;
+
+  await $db.put({
+    _id: currentLog.value.startedAt,
+    startedAt: currentLog.value.startedAt,
+    stoppedAt: new Date().toISOString(),
+    customerName: currentLog.value.customerName,
+    projectName: currentLog.value.projectName,
+  });
+  currentLog.value = undefined;
+};
 </script>
 
 <template>
@@ -16,6 +48,21 @@ const recentProjectsId = useId();
       >
         <div class="project-card__customer">{{ project.customerName }}</div>
         <div class="project-card__project">{{ project.projectName }}</div>
+        <OnyxIconButton
+          v-if="currentLog !== undefined && isCurrentLog(project)"
+          class="project-card__action-button"
+          label="Stop logging"
+          :icon="iconMediaStop"
+          @click="stopLogging()"
+        />
+        <OnyxIconButton
+          v-else
+          class="project-card__action-button"
+          label="Start logging"
+          :icon="iconMediaPlay"
+          :disabled="currentLog !== undefined"
+          @click="startLogging(project)"
+        />
       </OnyxCard>
     </div>
   </div>
@@ -41,15 +88,26 @@ const recentProjectsId = useId();
     gap: 0.5rem;
 
     .project-card {
+      display: grid;
       --onyx-card-gap: 0;
+      grid-template-columns: auto 48px;
+      grid-template-areas:
+        "customer action-button"
+        "project action-button";
 
       &__customer {
+        grid-area: customer;
         font-size: var(--onyx-font-size-sm);
       }
 
       &__project {
+        grid-area: project;
         font-size: var(--onyx-font-size-lg);
         font-weight: 500;
+      }
+
+      &__action-button {
+        grid-area: action-button;
       }
     }
   }
