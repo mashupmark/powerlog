72beaa369cb2b99735516a444ddb4b08bc881ee1
Mashupmark <mashupmark@kabelbw.de>
2026-05-23 16:28:20 +0200

    store selected project in query

--- app/pages/reports.vue
+++ app/pages/reports.vue
@@ -9,7 +9,17 @@ const projectsByCustomer = computed(() =>
   }, {}),
 );
 
-const currentSelection = shallowRef<{ customerName: string; projectName: string }>();
+const router = useRouter();
+
+const selection = computed(() => {
+  const { customer, project } = router.currentRoute.value.query;
+  if (!customer || typeof customer !== "string" || !project || typeof project !== "string") return undefined;
+  return { customer, project };
+});
+
+const changeSelection = async (customer: string, project: string) => {
+  await router.replace({ query: { customer, project } });
+};
 </script>
 
 <template>
@@ -21,14 +31,14 @@ const currentSelection = shallowRef<{ customerName: string; projectName: string 
         <OnyxAccordion :skeleton="isLoading">
           <OnyxAccordionItem v-for="(projects, customer) in projectsByCustomer" :key="customer" :value="customer">
             <template #header>
-              <div :class="{ 'customer--active': customer === currentSelection?.customerName }">{{ customer }}</div>
+              <div :class="{ 'customer--active': customer === selection?.customer }">{{ customer }}</div>
             </template>
 
             <OnyxMenuItem
               v-for="project in projects"
               :key="project"
-              :active="customer === currentSelection?.customerName && project === currentSelection?.projectName"
-              @click="currentSelection = { customerName: customer, projectName: project }"
+              :active="customer === selection?.customer && project === selection?.project"
+              @click="changeSelection(customer, project)"
             >
               {{ project }}
             </OnyxMenuItem>
@@ -36,6 +46,11 @@ const currentSelection = shallowRef<{ customerName: string; projectName: string 
         </OnyxAccordion>
       </OnyxSidebar>
     </template>
+
+    <NuxtPage v-if="selection" :customer="selection.customer" :project="selection.project" />
+    <div v-else class="empty">
+      <OnyxEmpty>Select a project on the left to see the report for it</OnyxEmpty>
+    </div>
   </OnyxPageLayout>
 </template>
 
@@ -44,6 +59,13 @@ const currentSelection = shallowRef<{ customerName: string; projectName: string 
   // Change the color of the customer accordion item of the currently selected project
   .customer--active {
     color: var(--onyx-color-text-icons-primary-bold);
+  }
+
+  .empty {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    place-items: center;
   }
 }
 </style>
