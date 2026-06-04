f429c8873ece620d8b328266fa17a8acf33d895d
mashupmark <46753823+mashupmark@users.noreply.github.com>
2026-06-04 16:18:41 +0200

    Cache fonts for offline availability (#14)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -70,7 +70,7 @@ export default defineNuxtConfig({
     },
     workbox: {
       navigateFallback: "/",
-      globPatterns: ["**/*.{js,css,html,json,svg,webp}"],
+      globPatterns: ["**/*.{js,css,html,json,svg,webp,woff2}"],
       globIgnores: ["/db/**"],
     },
   },
